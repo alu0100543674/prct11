8b39d6d9ee84092f3c6614c2e6885a441d633aef
alu0100543674 <Carla Hernandez>
2013-12-06 02:02:21 +0000

    Creadas matrices fraccionarias en el fichero spec

--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -10,6 +10,9 @@ describe Matriz do
       @matriz2 = matrizSuper.tipo([[2, 3], [4, 5]])
       @matriz3 = matrizSuper.tipo([[1, 1], [1, 1]])
      
+      # Matrices fraccionarias
+      @matrizFrac1 = matrizSuper.tipo([[Fraccion.new(1, 2), Fraccion.new(0, 1)], [Fraccion.new(0, 1), Fraccion.new(0, 1)]])
+      
       
    end
 end
