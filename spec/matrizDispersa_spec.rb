f1dab6e31cd5dbb4ca01f1f313e3e5f55ba475c4
alu0100543674 <Carla Hernandez>
2013-12-08 21:58:22 +0000

    Suma, multiplicacion, to_s y == corregido

--- spec/matrizDispersa_spec.rb
+++ spec/matrizDispersa_spec.rb
@@ -8,16 +8,21 @@ describe MatrizDispersa do
       # MATRICES ENTERAS
       # Dispersas
       @matriz3 = matrizSuper.tipo([[1, 0], [0, 0]])
-      @matriz4 = matrizSuper.tipo([[2, 0], [0, 0]])
+      @matriz4 = matrizSuper.tipo([[4, 0], [0, 0]])
       
 
       # Resultados
       @sumDispersa = matrizSuper.tipo([[3, 0], [0, 0]])
+		#@multDispersa = matrizSuper.tipo([[4, 0], [0, 0]])
+      @matrizComp = matrizSuper.tipo([[1, 0], [0, 0]])
+		
       
    end
 
    # Matrices Dispersas
-   describe "# Dispersa: Numero filas y columnas " do
+   
+
+  	describe "# Dispersa: Numero filas y columnas " do
    	it " Filas " do
    	   @matriz3.filas.should eq(2)
    	end
@@ -26,10 +31,25 @@ describe MatrizDispersa do
       end
    end
 
-   describe "# Dispersa: Suma" do
-   	it " Suma" do 
-      	sum = @matriz3 + @matriz4
-         sum.should == @sumDispersa
+   describe "# Operaciones " do
+	   it "Se deben poder sumar dos matrices" do
+         sum = (@matriz3 + @matriz4)
+         sum.to_s.should eq("5 0 \n0 0 \n")
+      end
+   
+		it " Multiplicacion " do
+			mult = @matriz3 * @matriz4
+			mult.to_s.should eq("8 0 \n0 0 \n")
+		end
+	end
+
+	describe "# Comprobando que la matriz no esta vacia" do
+   	it "La matriz debe contener datos" do
+    	  @matriz3.should == @matrizComp
       end
    end
+
+	
+
+
 end
